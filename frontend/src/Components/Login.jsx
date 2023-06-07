b6224708020e31bd2d31bca125ddc5b8e8c55b70
Raviranjan <ravikrcse2018@gmail.com>
2023-06-07 16:52:46 +0530

    working on login

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Login = () => {
+
+  const [email , setEmail] = useState('');
+const [password , setPassword] = useState('');
+
+const handleSubmit = () => {
+  const payload = {
+    email,
+    password
+  }
+  fetch(`http://localhost:8080/user/login`, {
+    method : "POST",
+    headers: {
+      'Content-Type' : 'application/json',
+    },
+    body : JSON.stringify(payload)
+  })
+  .then((res) => res.json())
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err))
+}
+
+
   return (
     <div>
-      
+      <input type="email" placeholder='email' onChange={(e) => setEmail(e.target.value)} />
+      <br/>
+      <input type="password" placeholder='password' onChange={(e) => setPassword(e.target.value)} />
+      <br />
+      <button onClick={handleSubmit}>Login</button>
     </div>
   )
 }
